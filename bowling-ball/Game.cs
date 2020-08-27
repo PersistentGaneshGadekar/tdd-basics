87197da72e17ff2454757ee9c0bbcfe46b29b065
PersistentGaneshGadekar <ganesh_gadekar@persistent.com>
2020-08-27 19:34:02 +0530

    game useing tdd

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,57 @@ namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll = 0;
+        private bool isSpare(int frameIndex)
         {
-            throw new NotImplementedException();
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
         }
 
-        public int GetScore()
+        private bool isStrike(int frameIndex)
         {
-            throw new NotImplementedException();
+            return rolls[frameIndex] == 10;
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (isStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+
+            }
+
+            return score;
         }
 
     }
